3e495261c2bd0c4148ba98287e7ee01d051a5c26
adller99 <adller99@gmail.com>
2022-10-16 15:50:15 +0200

    clone + java 17

---
+++ src/main/java/de/conrad/codeworkshop/factory/FactoryApplication.java
@@ -0,0 +1,17 @@
+package de.conrad.codeworkshop.factory;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static org.springframework.boot.SpringApplication.run;
+
+/**
+ * @author Andreas Hartmann
+ */
+@SpringBootApplication
+public class FactoryApplication {
+
+    public static void main(final String... args) {
+        run(FactoryApplication.class, args);
+    }
+
+}
